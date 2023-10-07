5e833d768df0ad685768bcb5154ce5e3b77da825
Luong The Tai <tailtph35654@fpt.edu.vn>
2023-10-07 21:00:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mob2014_luongthetai_ph35465/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mob2014_luongthetai_ph35465;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MOB2014_LuongTheTai_PH35465"
+include(":app")
+ 
